9b1fb5b82ae6f41002d3647ae4cda267daec3b0b
gousaiyang <gousaiyang@163.com>
2020-01-14 20:48:46 -0800

    Fix sys.path manipulation in test

--- test/test_predicate.py
+++ test/test_predicate.py
@@ -1,8 +1,10 @@
 import os
 import sys
-sys.path.append('..')
 
+ROOT = os.path.dirname(os.path.realpath(__file__))
+sys.path.insert(0, os.path.abspath(os.path.join(ROOT, '..')))
 import tbtrim
+sys.path.pop(0)
 
 import inner
 
